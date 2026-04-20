77a8788c00ef4376b329f1e8d692a266957effa5
zenmarmay06 <seyalinr@gmail.com>
2026-04-20 00:47:17 -0700

    Logic: Update room status to Occupied upon booking confirmation

--- admin/roomconfirm.php
+++ admin/roomconfirm.php
@@ -8,13 +8,13 @@ $sql ="Select * from roombook where id = '$id'";
 $re = mysqli_query($conn,$sql);
 while($row=mysqli_fetch_array($re))
 {
-	$Name = $row['Name'];
+    $Name = $row['Name'];
     $Email = $row['Email'];
     $Country = $row['Country'];
     $Phone = $row['Phone'];
     $RoomType = $row['RoomType'];
     $Bed = $row['Bed'];
-    $NoofRoom = $row['NoofRoom'];
+    $NoofRoom = $row['NoofRoom']; // Kini ang Room Number
     $Meal = $row['Meal'];
     $cin = $row['cin'];
     $cout = $row['cout'];
@@ -22,75 +22,43 @@ while($row=mysqli_fetch_array($re))
     $stat = $row['stat'];
 }
 
-
 if($stat == "NotConfirm")
 {
     $st = "Confirm";
 
+    // 1. I-update ang status sa booking
     $sql = "UPDATE roombook SET stat = '$st' WHERE id = '$id'";
     $result = mysqli_query($conn,$sql);
 
     if($result){
+        
+        // --- BAG-O: AUTOMATIC OCCUPIED LOGIC ---
+        // I-update ang status sa room table gamit ang room number ($NoofRoom)
+        $sql_room = "UPDATE room SET status = 'Occupied' WHERE room_no = '$NoofRoom'";
+        mysqli_query($conn, $sql_room);
+        // ----------------------------------------
 
         $type_of_room = 0;      
-        if($RoomType=="Superior Room")
-        {
-            $type_of_room = 3000;
-        }
-        else if($RoomType=="Deluxe Room")
-        {
-            $type_of_room = 2000;
-        }
-        else if($RoomType=="Guest House")
-        {
-            $type_of_room = 1500;
-        }
-        else if($RoomType=="Single Room")
-        {
-            $type_of_room = 1000;
-        }
+        if($RoomType=="Superior Room") { $type_of_room = 3000; }
+        else if($RoomType=="Deluxe Room") { $type_of_room = 2000; }
+        else if($RoomType=="Guest House") { $type_of_room = 1500; }
+        else if($RoomType=="Single Room") { $type_of_room = 1000; }
         
-        
-        if($Bed=="Single")
-        {
-            $type_of_bed = $type_of_room * 1/100;
-        }
-        else if($Bed=="Double")
-        {
-            $type_of_bed = $type_of_room * 2/100;
-        }
-        else if($Bed=="Triple")
-        {
-            $type_of_bed = $type_of_room * 3/100;
-        }
-        else if($Bed=="Quad")
-        {
-            $type_of_bed = $type_of_room * 4/100;
-        }
-            else if($Bed=="None")
-        {
-            $type_of_bed = $type_of_room * 0/100;
-        }
+        $type_of_bed = 0;
+        if($Bed=="Single") { $type_of_bed = $type_of_room * 1/100; }
+        else if($Bed=="Double") { $type_of_bed = $type_of_room * 2/100; }
+        else if($Bed=="Triple") { $type_of_bed = $type_of_bed = $type_of_room * 3/100; }
+        else if($Bed=="Quad") { $type_of_bed = $type_of_room * 4/100; }
+        else if($Bed=="None") { $type_of_bed = $type_of_room * 0/100; }
 
-        if($Meal=="Room only")
-        {
-            $type_of_meal=$type_of_bed * 0;
-        }
-        else if($Meal=="Breakfast")
-        {
-            $type_of_meal=$type_of_bed * 2;
-        }
-        else if($Meal=="Half Board")
-        {
-            $type_of_meal=$type_of_bed * 3;
-        }
-        else if($Meal=="Full Board")
-        {
-            $type_of_meal=$type_of_bed * 4;
-        }
-                                                            
-        $ttot = $type_of_room *  $noofday * $NoofRoom;
-        $mepr = $type_of_meal *  $noofday;
+        $type_of_meal = 0;
+        if($Meal=="Room only") { $type_of_meal=$type_of_bed * 0; }
+        else if($Meal=="Breakfast") { $type_of_meal=$type_of_bed * 2; }
+        else if($Meal=="Half Board") { $type_of_meal=$type_of_bed * 3; }
+        else if($Meal=="Full Board") { $type_of_meal=$type_of_bed * 4; }
+                                                                    
+        $ttot = $type_of_room * $noofday * 1; // Gi-multiply sa 1 room quantity
+        $mepr = $type_of_meal * $noofday;
         $btot = $type_of_bed * $noofday;
 
         $fintot = $ttot + $mepr + $btot;
@@ -102,11 +70,4 @@ if($stat == "NotConfirm")
         header("Location:roombook.php");
     }
 }
-// else
-// {
-//     echo "<script>alert('Guest Already Confirmed')</script>";
-//     header("Location:roombook.php");
-// }
-
-
 ?>
