69ad02f13d76147f640d80a30db74e5c8144d6e7
zenmarmay06 <seyalinr@gmail.com>
2026-04-20 00:57:18 -0700

    Update: Improve room booking management and confirmation workflow

--- admin/roombook.php
+++ admin/roombook.php
@@ -11,20 +11,16 @@ include '../config.php';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- boot -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- fontowesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <!-- sweet alert -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="./css/roombook.css">
     <title>BlueBird - Admin</title>
 </head>
 
 <body>
-    <!-- guestdetailpanel -->
-
     <div id="guestdetailpanel">
         <form action="" method="POST" class="guestdetailpanelform">
             <div class="head">
@@ -42,13 +38,12 @@ include '../config.php';
                     ?>
 
                     <select name="Country" class="selectinput" required>
-						<option value selected >Select your country</option>
+                        <option value selected>Select your country</option>
                         <?php
-							foreach($countries as $key => $value):
-							echo '<option value="'.$value.'">'.$value.'</option>';
-                            //close your tags!!
-							endforeach;
-						?>
+                        foreach ($countries as $key => $value):
+                            echo '<option value="' . $value . '">' . $value . '</option>';
+                        endforeach;
+                        ?>
                     </select>
                     <input type="text" name="Phone" placeholder="Enter Phoneno" required>
                 </div>
@@ -57,42 +52,37 @@ include '../config.php';
 
                 <div class="reservationinfo">
                     <h4>Reservation information</h4>
-                    <select name="RoomType" class="selectinput">
-						<option value selected >Type Of Room</option>
+                    <select name="RoomType" id="RoomType" class="selectinput" onchange="fetchDetails(this.value)" required>
+                        <option value selected disabled>Type Of Room</option>
                         <option value="Superior Room">SUPERIOR ROOM</option>
                         <option value="Deluxe Room">DELUXE ROOM</option>
-						<option value="Guest House">GUEST HOUSE</option>
-						<option value="Single Room">SINGLE ROOM</option>
+                        <option value="Guest House">GUEST HOUSE</option>
+                        <option value="Single Room">SINGLE ROOM</option>
                     </select>
-                    <select name="Bed" class="selectinput">
-						<option value selected >Bedding Type</option>
-                        <option value="Single">Single</option>
-                        <option value="Double">Double</option>
-						<option value="Triple">Triple</option>
-                        <option value="Quad">Quad</option>
-						<option value="None">None</option>
+
+                    <select name="Bed" id="Bed" class="selectinput" required>
+                        <option value selected disabled>Bedding Type</option>
                     </select>
-                    <select name="NoofRoom" class="selectinput">
-						<option value selected >No of Room</option>
-                        <option value="1">1</option>
-                        <!-- <option value="1">2</option>
-                        <option value="1">3</option> -->
+
+                    <select name="NoofRoom" id="NoofRoom" class="selectinput" required>
+                        <option value selected disabled>Select Room No</option>
                     </select>
-                    <select name="Meal" class="selectinput">
-						<option value selected >Meal</option>
+
+                    <select name="Meal" class="selectinput" required>
+                        <option value selected disabled>Meal</option>
                         <option value="Room only">Room only</option>
                         <option value="Breakfast">Breakfast</option>
-						<option value="Half Board">Half Board</option>
-						<option value="Full Board">Full Board</option>
-					</select>
+                        <option value="Half Board">Half Board</option>
+                        <option value="Full Board">Full Board</option>
+                    </select>
                     <div class="datesection">
                         <span>
                             <label for="cin"> Check-In</label>
-                            <input name="cin" type ="date">
+                            <input name="cin" type="date" required>
                         </span>
                         <span>
-                            <label for="cin"> Check-Out</label>
-                            <input name="cout" type ="date">
+                            <label for="cout"> Check-Out</label>
+                            <input name="cout" type="date" required>
                         </span>
                     </div>
                 </div>
@@ -102,184 +92,37 @@ include '../config.php';
             </div>
         </form>
 
-        <?php       
-        // <!-- room availablity start-->
+        <?php
+        if (isset($_POST['guestdetailsubmit'])) {
+            $Name = $_POST['Name'];
+            $Email = $_POST['Email'];
+            $Country = $_POST['Country'];
+            $Phone = $_POST['Phone'];
+            $RoomType = $_POST['RoomType'];
+            $Bed = $_POST['Bed'];
+            $NoofRoom = $_POST['NoofRoom'];
+            $Meal = $_POST['Meal'];
+            $cin = $_POST['cin'];
+            $cout = $_POST['cout'];
 
-        $rsql ="select * from room";
-        $rre= mysqli_query($conn,$rsql);
-        $r = 0;
-        $sc = 0;
-        $gh = 0;
-        $sr = 0;
-        $dr = 0;
+            if ($Name == "" || $Email == "" || $Country == "") {
+                echo "<script>swal({title: 'Fill the proper details', icon: 'error'});</script>";
+            } else {
+                $sta = "NotConfirm";
+                $sql = "INSERT INTO roombook(Name,Email,Country,Phone,RoomType,Bed,NoofRoom,Meal,cin,cout,stat,nodays) 
+                        VALUES ('$Name','$Email','$Country','$Phone','$RoomType','$Bed','$NoofRoom','$Meal','$cin','$cout','$sta',datediff('$cout','$cin'))";
+                $result = mysqli_query($conn, $sql);
 
-        while($rrow=mysqli_fetch_array($rre))
-        {
-            $r = $r + 1;
-            $s = $rrow['type'];
-            if($s=="Superior Room")
-            {
-                $sc = $sc+ 1;
-            }
-            if($s=="Guest House")
-            {
-                $gh = $gh + 1;
-            }
-            if($s=="Single Room" )
-            {
-                $sr = $sr + 1;
-            }
-            if($s=="Deluxe Room" )
-            {
-                $dr = $dr + 1;
-            }
-        }
-
-        $csql ="select * from payment";
-        $cre= mysqli_query($conn,$csql);
-        $cr =0 ;
-        $csc =0;
-        $cgh = 0;
-        $csr = 0;
-        $cdr = 0;
-        while($crow=mysqli_fetch_array($cre))
-        {
-            $cr = $cr + 1;
-            $cs = $crow['RoomType'];
-                        
-            if($cs=="Superior Room")
-            {
-                $csc = $csc + 1;
-            }
-                        
-            if($cs=="Guest House" )
-            {
-                $cgh = $cgh + 1;
-            }
-            if($cs=="Single Room")
-            {
-                $csr = $csr + 1;
-            }
-            if($cs=="Deluxe Room")
-            {
-                $cdr = $cdr + 1;
-            }
-        }
-        // room availablity
-        // Superior Room =>
-        $f1 =$sc - $csc;
-        if($f1 <=0 )
-        {	
-            $f1 = "NO";
-        }
-        // Guest House =>
-        $f2 =  $gh -$cgh;
-        if($f2 <=0 )
-        {	
-            $f2 = "NO";
-        }
-        // Single Room =>
-        $f3 =$sr - $csr;
-        if($f3 <=0 )
-        {	
-            $f3 = "NO";
-        }
-        // Deluxe Room =>
-        $f4 =$dr - $cdr; 
-        if($f4 <=0 )
-        {	
-            $f4 = "NO";
-        }
-        //total available room =>
-        $f5 =$r-$cr; 
-        if($f5 <=0 )
-        {
-            $f5 = "NO";
-        }
-        ?>
-        <!-- room availablity end-->
-
-        <!-- ==== room book php ====-->
-        <?php       
-            if (isset($_POST['guestdetailsubmit'])) {
-                $Name = $_POST['Name'];
-                $Email = $_POST['Email'];
-                $Country = $_POST['Country'];
-                $Phone = $_POST['Phone'];
-                $RoomType = $_POST['RoomType'];
-                $Bed = $_POST['Bed'];
-                $NoofRoom = $_POST['NoofRoom'];
-                $Meal = $_POST['Meal'];
-                $cin = $_POST['cin'];
-                $cout = $_POST['cout'];
-
-                if($Name == "" || $Email == "" || $Country == ""){
-                    echo "<script>swal({
-                        title: 'Fill the proper details',
-                        icon: 'error',
-                    });
-                    </script>";
-                }
-                else{
-                    $sta = "NotConfirm";
-                    $sql = "INSERT INTO roombook(Name,Email,Country,Phone,RoomType,Bed,NoofRoom,Meal,cin,cout,stat,nodays) VALUES ('$Name','$Email','$Country','$Phone','$RoomType','$Bed','$NoofRoom','$Meal','$cin','$cout','$sta',datediff('$cout','$cin'))";
-                    $result = mysqli_query($conn, $sql);
-
-                    // if($f1=="NO")
-                    // {
-                    //     echo "<script>swal({
-                    //         title: 'Superior Room is not available',
-                    //         icon: 'error',
-                    //     });
-                    //     </script>";
-                    // }
-                    // else if($f2=="NO")
-                    // {
-                    //     echo "<script>swal({
-                    //         title: 'Guest House is not available',
-                    //         icon: 'error',
-                    //     });
-                    //     </script>";
-                    // }
-                    // else if($f3 == "NO")
-                    // {
-                    //     echo "<script>swal({
-                    //         title: 'Si Room is not available',
-                    //         icon: 'error',
-                    //     });
-                    //     </script>";
-                    // }
-                    // else if($f4 == "NO")
-                    // {
-                    //     echo "<script>swal({
-                    //         title: 'Deluxe Room is not available',
-                    //         icon: 'error',
-                    //     });
-                    //     </script>";
-                    // }
-                    // else if($result = mysqli_query($conn, $sql))
-                    // {
-                        if ($result) {
-                            echo "<script>swal({
-                                title: 'Reservation successful',
-                                icon: 'success',
-                            });
-                        </script>";
-                        } else {
-                            echo "<script>swal({
-                                    title: 'Something went wrong',
-                                    icon: 'error',
-                                });
-                        </script>";
-                        }
-                    // }
+                if ($result) {
+                    echo "<script>swal({title: 'Reservation successful', icon: 'success'});</script>";
+                } else {
+                    echo "<script>swal({title: 'Something went wrong', icon: 'error'});</script>";
                 }
             }
+        }
         ?>
     </div>
 
-    
-    <!-- ================================================= -->
     <div class="searchsection">
         <input type="text" name="search_bar" id="search_bar" placeholder="search..." onkeyup="searchFun()">
         <button class="adduser" id="adduser" onclick="adduseropen()"><i class="fa-solid fa-bookmark"></i> Add</button>
@@ -290,9 +133,8 @@ include '../config.php';
 
     <div class="roombooktable" class="table-responsive-xl">
         <?php
-            $roombooktablesql = "SELECT * FROM roombook";
-            $roombookresult = mysqli_query($conn, $roombooktablesql);
-            $nums = mysqli_num_rows($roombookresult);
+        $roombooktablesql = "SELECT * FROM roombook";
+        $roombookresult = mysqli_query($conn, $roombooktablesql);
         ?>
         <table class="table table-bordered" id="table-data">
             <thead>
@@ -304,59 +146,91 @@ include '../config.php';
                     <th scope="col">Phone</th>
                     <th scope="col">Type of Room</th>
                     <th scope="col">Type of Bed</th>
-                    <th scope="col">No of Room</th>
+                    <th scope="col">Room No</th>
                     <th scope="col">Meal</th>
                     <th scope="col">Check-In</th>
                     <th scope="col">Check-Out</th>
                     <th scope="col">No of Day</th>
                     <th scope="col">Status</th>
                     <th scope="col" class="action">Action</th>
-                    <!-- <th>Delete</th> -->
                 </tr>
             </thead>
 
             <tbody>
-            <?php
-            while ($res = mysqli_fetch_array($roombookresult)) {
-            ?>
-                <tr>
-                    <td><?php echo $res['id'] ?></td>
-                    <td><?php echo $res['Name'] ?></td>
-                    <td><?php echo $res['Email'] ?></td>
-                    <td><?php echo $res['Country'] ?></td>
-                    <td><?php echo $res['Phone'] ?></td>
-                    <td><?php echo $res['RoomType'] ?></td>
-                    <td><?php echo $res['Bed'] ?></td>
-                    <td><?php echo $res['NoofRoom'] ?></td>
-                    <td><?php echo $res['Meal'] ?></td>
-                    <td><?php echo $res['cin'] ?></td>
-                    <td><?php echo $res['cout'] ?></td>
-                    <td><?php echo $res['nodays'] ?></td>
-                    <td><?php echo $res['stat'] ?></td>
-                    <td class="action">
-                        <?php
-                            if($res['stat'] == "Confirm")
-                            {
-                                echo " ";
-                            }
-                            else
-                            {
-                                echo "<a href='roomconfirm.php?id=". $res['id'] ."'><button class='btn btn-success'>Confirm</button></a>";
-                            }
-                        ?>
-                        <a href="roombookedit.php?id=<?php echo $res['id'] ?>"><button class="btn btn-primary">Edit</button></a>
-                        <a href="roombookdelete.php?id=<?php echo $res['id'] ?>"><button class='btn btn-danger'>Delete</button></a>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
+                <?php
+                while ($res = mysqli_fetch_array($roombookresult)) {
+                    $status = $res['stat'];
+                ?>
+                    <tr>
+                        <td><?php echo $res['id'] ?></td>
+                        <td><?php echo $res['Name'] ?></td>
+                        <td><?php echo $res['Email'] ?></td>
+                        <td><?php echo $res['Country'] ?></td>
+                        <td><?php echo $res['Phone'] ?></td>
+                        <td><?php echo $res['RoomType'] ?></td>
+                        <td><?php echo $res['Bed'] ?></td>
+                        <td><?php echo $res['NoofRoom'] ?></td>
+                        <td><?php echo $res['Meal'] ?></td>
+                        <td><?php echo $res['cin'] ?></td>
+                        <td><?php echo $res['cout'] ?></td>
+                        <td><?php echo $res['nodays'] ?></td>
+                        <td><?php echo $res['stat'] ?></td>
+                        <td class="action">
+    <?php
+    $status = $res['stat']; 
+
+    if ($status !== "Checked out") {
+        // Confirm Button
+        if ($status == "NotConfirm") {
+            echo "<a href='roomconfirm.php?id=" . $res['id'] . "'><button class='btn btn-success btn-sm'>Confirm</button></a> ";
+        }
+
+        // EDIT Button
+        echo "<a href='roombookedit.php?id=" . $res['id'] . "'><button class='btn btn-primary btn-sm'>Edit</button></a> ";
+
+        // BAG-O: CHECKOUT Button (mo-diretso sa checkout process)
+        echo "<a href='roombookcheckout.php?id=" . $res['id'] . "' onclick='return confirm(\"Finalize check-out for this guest?\")'>
+                <button class='btn btn-warning btn-sm'>Checkout</button>
+              </a> ";
+
+        // Delete Button
+        echo "<a href='roombookdelete.php?id=" . $res['id'] . "'><button class='btn btn-danger btn-sm'>Delete</button></a>";
+    } else {
+        echo "<span class='badge bg-secondary' style='padding: 8px;'>No Action Available</span>";
+    }
+    ?>
+</td>
+                    </tr>
+                <?php
+                }
+                ?>
             </tbody>
         </table>
     </div>
+
+    <script>
+        function fetchDetails(roomType) {
+            if (roomType == "") {
+                $('#Bed').html('<option value="">Select Bedding Type</option>');
+                $('#NoofRoom').html('<option value="">Select Room No</option>');
+                return;
+            }
+
+            $.ajax({
+                url: 'fetch_room_details.php', // Kinahanglan buhaton ni nimo nga file
+                method: 'POST',
+                data: {
+                    room_type: roomType
+                },
+                dataType: 'JSON',
+                success: function(data) {
+                    $('#Bed').html(data.beds);
+                    $('#NoofRoom').html(data.rooms);
+                }
+            });
+        }
+    </script>
 </body>
 <script src="./javascript/roombook.js"></script>
-
-
 
 </html>
