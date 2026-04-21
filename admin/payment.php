78b55cfc796e4fe73e122e643558259e61ac6f96
zenmarmay06 <seyalinr@gmail.com>
2026-04-20 23:40:28 -0700

    feat(payment): implement AJAX auto refresh

--- admin/payment.php
+++ admin/payment.php
@@ -2,7 +2,52 @@
     session_start();
     include '../config.php';
 
+    // --- KINI NGA PARTE PARA SA AUTO-FETCH (AJAX) ---
+    if (isset($_GET['action']) && $_GET['action'] == 'fetch_data') {
+        $paymanttablesql = "SELECT * FROM payment ORDER BY id DESC";
+        $paymantresult = mysqli_query($conn, $paymanttablesql);
 
+        while ($res = mysqli_fetch_array($paymantresult)) {
+            $roomType = $res['RoomType'];
+            $bedType  = $res['Bed'];
+            $reservationPrice = 0;
+
+            if (strcasecmp($roomType, "Superior Room") == 0) {
+                if ($bedType == "Single")      $reservationPrice = 1000;
+                elseif ($bedType == "Double")  $reservationPrice = 2000;
+                elseif ($bedType == "Triple")  $reservationPrice = 2500;
+                elseif ($bedType == "Quad")    $reservationPrice = 3000;
+            } 
+            elseif (strcasecmp($roomType, "Deluxe Room") == 0) {
+                if ($bedType == "Single")      $reservationPrice = 4000;
+                elseif ($bedType == "Double")  $reservationPrice = 4800;
+                elseif ($bedType == "Triple")  $reservationPrice = 4900;
+                elseif ($bedType == "Quad")    $reservationPrice = 5000;
+            } 
+           
+
+            $days = ($res['noofdays'] <= 0) ? 1 : $res['noofdays'];
+            $totalBill = $reservationPrice * $days;
+
+            echo "<tr>
+                    <td>{$res['id']}</td>
+                    <td>{$res['Name']}</td>
+                    <td>{$res['RoomType']}</td>
+                    <td>{$res['Bed']}</td>
+                    <td>{$res['cin']}</td>
+                    <td>{$res['cout']}</td>
+                    <td>{$res['noofdays']}</td>
+                    <td>{$res['NoofRoom']}</td>
+                    <td class='fw-bold'>₱ " . number_format($reservationPrice, 2) . "</td>
+                    <td class='text-success fw-bold'>₱ " . number_format($totalBill, 2) . "</td>
+                    <td class='action'>
+                        <a href='invoiceprint.php?id={$res['id']}' class='btn btn-primary btn-sm'><i class='fa-solid fa-print'></i> Print</a>
+                        <a href='paymantdelete.php?id={$res['id']}' class='btn btn-danger btn-sm' onclick='return confirm(\"Delete?\")'><i class='fa-solid fa-trash'></i> Delete</a>
+                    </td>
+                  </tr>";
+        }
+        exit; // Hunongon ang execution para dili ma-load ang tibuok HTML
+    }
 ?>
 
 <!DOCTYPE html>
@@ -11,105 +56,69 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" />
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <link rel="stylesheet" href="./css/roombook.css">
     <title>BlueBird - Admin</title>
-    <!-- boot -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-    <!-- fontowesome -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer"/>
-	<!-- css for table and search bar -->
-	<link rel="stylesheet" href="css/roombook.css">
-
 </head>
 <body>
-	<div class="searchsection">
-        <input type="text" name="search_bar" id="search_bar" placeholder="search..." onkeyup="searchFun()">
+
+    <div class="searchsection">
+        <input type="text" id="search_bar" placeholder="Search customer name..." onkeyup="searchFun()">
     </div>
 
-    <div class="roombooktable" class="table-responsive-xl">
-        <?php
-            $paymanttablesql = "SELECT * FROM payment";
-            $paymantresult = mysqli_query($conn, $paymanttablesql);
-
-            $nums = mysqli_num_rows($paymantresult);
-        ?>
-        <table class="table table-bordered" id="table-data">
-            <thead>
+    <div class="roombooktable table-responsive-xl">
+        <table class="table table-bordered table-striped" id="table-data">
+            <thead class="table-dark">
                 <tr>
-                    <th scope="col">Id</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Room Type</th>
-                    <th scope="col">Bed Type</th>
-                    <th scope="col">Check In</th>
-                    <th scope="col">Check In</th>
-					<th scope="col">No of Day</th>
-                    <th scope="col">No of Room</th>
-					<th scope="col">Meal Type</th>
-                    <th scope="col">Room Rent</th>
-                    <th scope="col">Bed Rent</th>
-                    <th scope="col">Meals</th>
-					<th scope="col">Total Bill</th>
-                    <th scope="col">Action</th>
-                    <!-- <th>Delete</th> -->
+                    <th>Id</th><th>Name</th><th>Room Type</th><th>Bed Type</th>
+                    <th>Check In</th><th>Check Out</th><th>No. of Days</th><th>No. of Rooms</th>
+                    <th>Reservation Price</th><th>Total Bill</th><th>Action</th>
                 </tr>
             </thead>
-
-            <tbody>
-            <?php
-            while ($res = mysqli_fetch_array($paymantresult)) {
-            ?>
-                <tr>
-                    <td><?php echo $res['id'] ?></td>
-                    <td><?php echo $res['Name'] ?></td>
-                    <td><?php echo $res['RoomType'] ?></td>
-                    <td><?php echo $res['Bed'] ?></td>
-					<td><?php echo $res['cin'] ?></td>
-                    <td><?php echo $res['cout'] ?></td>
-					<td><?php echo $res['noofdays'] ?></td>
-                    <td><?php echo $res['NoofRoom'] ?></td>
-                    <td><?php echo $res['meal'] ?></td>
-                    <td><?php echo $res['roomtotal'] ?></td>
-					<td><?php echo $res['bedtotal'] ?></td>
-					<td><?php echo $res['mealtotal'] ?></td>
-					<td><?php echo $res['finaltotal'] ?></td>
-                    <td class="action">
-                        <a href="invoiceprint.php?id= <?php echo $res['id']?>"><button class="btn btn-primary"><i class="fa-solid fa-print"></i>Print</button></a>
-						<a href="paymantdelete.php?id=<?php echo $res['id']?>"><button class="btn btn-danger">Delete</button></a>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-            </tbody>
+            <tbody id="show_data">
+                </tbody>
         </table>
     </div>
-</body>
 
-<script>
-    //search bar logic using js
-    const searchFun = () =>{
-        let filter = document.getElementById('search_bar').value.toUpperCase();
-
-        let myTable = document.getElementById("table-data");
-
-        let tr = myTable.getElementsByTagName('tr');
-
-        for(var i = 0; i< tr.length;i++){
-            let td = tr[i].getElementsByTagName('td')[1];
-
-            if(td){
-                let textvalue = td.textContent || td.innerHTML;
-
-                if(textvalue.toUpperCase().indexOf(filter) > -1){
-                    tr[i].style.display = "";
-                }else{
-                    tr[i].style.display = "none";
-                }
+    <script>
+        // FUNCTION PARA I-FETCH ANG DATA
+        function fetchData() {
+            let filter = document.getElementById('search_bar').value;
+            // Dili nato i-refresh kung naay sulod ang search bar para dili ma-interrupt ang admin
+            if (filter === "") {
+                $.ajax({
+                    url: "payment.php?action=fetch_data",
+                    type: "GET",
+                    success: function(data) {
+                        $('#show_data').html(data);
+                    }
+                });
             }
         }
 
-    }
+        // LOAD DAYON INAG ABRI SA PAGE
+        $(document).ready(function() {
+            fetchData();
+            // MAG-REFRESH MATAG 3 KA SEGUNDO (Polling)
+            setInterval(fetchData, 3000);
+        });
 
-</script>
-
+        // SEARCH FUNCTION
+        function searchFun() {
+            let filter = document.getElementById('search_bar').value.toUpperCase();
+            let tr = document.getElementById("show_data").getElementsByTagName('tr');
+            for (let i = 0; i < tr.length; i++) {
+                let tdName = tr[i].getElementsByTagName('td')[1];
+                if (tdName) {
+                    let textvalue = tdName.textContent || tdName.innerText;
+                    tr[i].style.display = (textvalue.toUpperCase().indexOf(filter) > -1) ? "" : "none";
+                }
+            }
+        }
+    </script>
+</body>
 </html>
